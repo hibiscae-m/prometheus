5fa7161e344af685d87834bf2f682e844265c4b7
Hibiscae <111366072+hibiscae-m@users.noreply.github.com>
2023-03-23 17:41:43 +0100

    Update make_as_a_service.ps1

--- windows_exporter/make_as_a_service.ps1
+++ windows_exporter/make_as_a_service.ps1
@@ -1,2 +1,2 @@
-New-Service -Name "windows_exporter" -DisplayName "Windows Exporter" -Description "Exposition des metrics pour Prometheus" -StartupType AutomaticDelayedStart -BinaryPathName '"C:\Program Files\windows_exporter\windows_exporter-0.21.0-amd64.exe" --config.file="C:\Program Files\windows_exporter\config.yml" --web.config.file="C:\Program Files\windows_exporter\web-config.yml"'
+New-Service -Name "windows_exporter" -DisplayName "Windows Exporter" -Description "Exposition des metrics pour Prometheus" -StartupType Automatic -BinaryPathName '"C:\Program Files\windows_exporter\windows_exporter-0.21.0-amd64.exe" --config.file="C:\Program Files\windows_exporter\config.yml" --web.config.file="C:\Program Files\windows_exporter\web-config.yml"'
 Start-Service -Name "windows_exporter"
